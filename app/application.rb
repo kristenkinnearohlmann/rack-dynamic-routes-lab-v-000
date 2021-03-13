ef01318cec8b191c914e49689c4d89cbfe4d6091
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-03-13 03:14:15 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
     resp = Rack::Response.new
 
     if req.path.match(/items/)
-      item = rew.path.split("/items/").last
+      item = req.path.split("/items/").last
       binding.pry
     else
       resp.status = 404
